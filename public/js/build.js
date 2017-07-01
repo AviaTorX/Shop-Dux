3b38ccfe384702aa0c7e6ca268243356fcc20e33
AviaTorX <shinde91avinash@gmail.com>
2017-07-01 13:49:21 +0530

    Lecture 5 updates

--- public/js/build.js
+++ public/js/build.js
@@ -702,14 +702,21 @@ function compose() {
 var _redux = __webpack_require__(8);
 
 var reducer = function reducer() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var action = arguments[1];
 
   switch (action.type) {
-    case "INCREMENT":
-      return state + action.payload;
+    // case "INCREMENT":
+    //   return state + action.payload;
+    //   break;
+
+    // case "DECREMENT":
+    //   return state - action.payload;
+    //   break;
+    // default:
+    case "POST_BOOK":
+      return state = action.payload;
       break;
-    default:
 
   }
   return state;
@@ -721,7 +728,17 @@ store.subscribe(function () {
   console.log('current state : ', store.getState());
 });
 
-store.dispatch({ type: "INCREMENT", payload: 1 });
+// store.dispatch({type:"INCREMENT", payload:1})
+// store.dispatch({type:"INCREMENT", payload:1})
+// store.dispatch({type:"DECREMENT", payload:1})
+// store.dispatch({type:"INCREMENT", payload:1})
+
+store.dispatch({ type: "POST_BOOK", payload: {
+    id: 1,
+    title: "WElcome",
+    description: "Learning",
+    price: 33
+  } });
 
 /***/ }),
 /* 8 */
--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import {createStore} from 'redux';
 
-var reducer = function(state=0, action){
+var reducer = function(state={}, action){
   switch (action.type) {
-    case "INCREMENT":
-      return state + action.payload;
+    // case "INCREMENT":
+    //   return state + action.payload;
+    //   break;
+
+    // case "DECREMENT":
+    //   return state - action.payload;
+    //   break;
+    // default:
+    case "POST_BOOK":
+      return state = action.payload;
       break;
-    default:
 
   }
   return state;
@@ -18,4 +25,14 @@ store.subscribe(function(){
 })
 
 
-store.dispatch({type:"INCREMENT", payload:1})
+// store.dispatch({type:"INCREMENT", payload:1})
+// store.dispatch({type:"INCREMENT", payload:1})
+// store.dispatch({type:"DECREMENT", payload:1})
+// store.dispatch({type:"INCREMENT", payload:1})
+
+store.dispatch({type:"POST_BOOK", payload:{
+  id:1,
+  title:"WElcome",
+  description:"Learning",
+  price:33
+}})
